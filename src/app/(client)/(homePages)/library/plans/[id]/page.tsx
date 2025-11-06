5ab79a54cb5bdae68c1ab2f053f64094ba855277
SigveDev <iam2derp4you@gmail.com>
2025-11-06 22:42:14 +0100

    test: trying to use any as type for params in planpage

--- src/app/(client)/(homePages)/library/plans/[id]/page.tsx
+++ src/app/(client)/(homePages)/library/plans/[id]/page.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import PlanPageComponent from "./planPageComponent";
 
-export default async function PlanPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function PlanPage({ params }: { params: any }) {
+  const { id } = await params;
 
   return <PlanPageComponent id={id} />;
 }
